e299e9139368ff48ca76a36e7e3d6c11f7134af0
Tejas Ambhore <tejasam2020@gmail.com>
2022-12-28 20:54:02 +0530

    header

--- src/containers/Header/headerContainer.js
+++ src/containers/Header/headerContainer.js
@@ -3,12 +3,12 @@ const Header = () => {
     return ( 
         <div class="header">
                 <div class="hallName">
-                    RADHAKRISHNAN HALL OF RESIDANCE
+                    RADHAKRISHNAN HALL OF RESIDENCE
                 </div>
                 <div class="hallDescription">
                     Radhakrishnan Hall of Residence, or the R.K. Hall, is named after the renowned scholar and statesman, Dr. Sarvapalli Radhakrishnan, who served as the honored President of India from 1962 to 1967.
                 </div>
-                <a href=""><button class="headerReadMore">
+                <a href="/"><button class="headerReadMore">
                     <b>Read More &gt;</b>
                 </button></a>
 
