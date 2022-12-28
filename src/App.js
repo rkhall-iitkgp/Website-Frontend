06fb652085fa482da18e8253e688a30ca4df56d3
Tejas Ambhore <tejasam2020@gmail.com>
2022-12-28 19:58:58 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      Hey There!
+    <div>
+      <Navbar />
     </div>
   );
 }
