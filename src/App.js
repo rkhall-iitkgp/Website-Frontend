863fb4c270f2fdfa3cd9d4ec79d8df1cc2ed03b9
Aman <84221394+aman7488@users.noreply.github.com>
2022-12-29 17:27:38 +0530

    updated Testimonials.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
+<<<<<<< Updated upstream
 import Gallery from "./components/Home_Gallery/galleryHome.js";
 
 function App() {
   return (
     <div className="App">
       <Gallery></Gallery>
+=======
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./containers/Header/headerContainer";
+import GalleryHome from "./components/Home_Gallery/galleryHome";
+import AboutUs from "./components/Home_About/About";
+import Testimonals from "./components/Testimonals/Testimonals";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <GalleryHome />
+      <AboutUs />
+      <Testimonals />
+>>>>>>> Stashed changes
     </div>
   );
 }
