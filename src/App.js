7407f55b0259d26263f4b960ac46a62db24fc885
Aman <84221394+aman7488@users.noreply.github.com>
2023-01-03 21:53:52 +0530

    added mobile view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import Navbar from './components/Navbar/Navbar';
-import Header from './containers/Header/headerContainer';
-import GalleryHome from './components/Home_Gallery/galleryHome';
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./containers/Header/headerContainer";
+import GalleryHome from "./components/Home_Gallery/galleryHome";
 import AboutUs from "./components/Home_About/About";
 
 function App() {
