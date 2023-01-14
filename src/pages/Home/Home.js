60bfe394303dc0baf60f9a11ec44a859f7c5d5b2
Aman <84221394+aman7488@users.noreply.github.com>
2023-01-14 22:50:41 +0530

    responsive testimonials

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,24 +1,24 @@
-import Navbar from "../../components/Navbar/Navbar";
-import Header from "../../containers/Header/headerContainer";
-import GalleryHome from "../../components/Home_Gallery/galleryHome";
-import AboutUs from "../../components/Home_About/About";
+// import Navbar from "../../components/Navbar/Navbar";
+// import Header from "../../containers/Header/headerContainer";
+// import GalleryHome from "../../components/Home_Gallery/galleryHome";
+// import AboutUs from "../../components/Home_About/About";
 import Testimonials from "../../components/Testimonials/Testimonials";
-import ProfileList from "../../components/Office_Bearers/ProfileList";
-import Footer from "../../components/Footer/Footer"
+// import ProfileList from "../../components/Office_Bearers/ProfileList";
+// import Footer from "../../components/Footer/Footer"
 import './css/Home.css'
 
 function Home() {
     return (
         <>
-            <Navbar />
+            {/* <Navbar />
             <Header />
             <GalleryHome />
-            <AboutUs />
+            <AboutUs /> */}
             <Testimonials />
             {/* <div className='profile-container'>
                 <ProfileList />
             </div> */}
-            <Footer />
+            {/* <Footer /> */}
         </>
     );
 }
