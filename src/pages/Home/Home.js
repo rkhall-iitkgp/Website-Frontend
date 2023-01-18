0065d6d90bc39326b47a554ceaa4a1ee793f3dc3
Aman <84221394+aman7488@users.noreply.github.com>
2023-01-18 22:03:46 +0530

    fixed aboutUs

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function Home() {
     return (
         <>
@@ -7,3 +8,38 @@ function Home() {
 }
     
 export default Home;
+=======
+import Navbar from "../../components/Navbar/Navbar";
+import Header from "../../containers/Header/headerContainer";
+import GalleryHome from "../../components/Home_Gallery/galleryHome";
+import AboutUs from "../../components/Home_About/About";
+import Testimonials from "../../components/Testimonials/Testimonials";
+import WardenProfileList from "../../components/Office_Bearers/Wardens/ProfileListWarden";
+import HeadProfileList from "../../components/Office_Bearers/Heads/ProfileListHeads";
+import GsecProfileList from "../../components/Office_Bearers/Gsec/ProfileListGsec";
+import Footer from "../../components/Footer/Footer";
+import "./css/Home.css";
+import wardenprofiles from "../../components/Office_Bearers/Wardens/profiles.json";
+import headprofiles from "../../components/Office_Bearers/Heads/profiles.json";
+import gsecprofiles from "../../components/Office_Bearers/Gsec/profiles.json";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <GalleryHome />
+      <AboutUs />
+      <Testimonials />
+      <div className="profile-container">
+        <WardenProfileList profiles={wardenprofiles} />
+        <HeadProfileList profiles={headprofiles} />
+        <GsecProfileList profiles={gsecprofiles} />
+      </div>
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default Home;
+>>>>>>> Stashed changes
