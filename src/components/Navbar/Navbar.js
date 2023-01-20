a6d3d16f18e9d7ad70623f4aa435dcda1531563c
Adarsh-51 <108488189+Adarsh-51@users.noreply.github.com>
2023-01-20 20:40:44 +0530

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,24 +6,43 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { useNavigate } from 'react-router-dom';
+import DrawerComp from './DrawerComp';
+import { useMediaQuery } from '@material-ui/core';
+import {
+  useTheme,
+} from "@material-ui/core";
+import Logo from './rklogo_1.png';
+
 
 export default function ButtonAppBar() {
     const Navigate = useNavigate(); 
   
+      const theme = useTheme();
+      const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+
+
   return (
     <Box sx={{ flexGrow: 1 }}>
         <CssBaseline/>
       <AppBar position="static" style={{ background: '#232536' }}>
         <Toolbar>
+          
           <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} >
-            R K H
+          <img src={Logo} alt="RKH" style={{maxWidth: 30, marginRight: '10px'}}/>
           </Typography>
-          <Button color="inherit" onClick={() => Navigate('/')}>Home</Button>
-          <Button color="inherit" onClick={() => Navigate('/gallery')}>Gallery</Button>
-          <Button color="inherit" onClick={() => Navigate('/alumni')}>Alumni</Button>
-          <Button color="inherit">About Us</Button>
-          <Button color="inherit">Testimonials</Button>
-          <Button color="inherit">Contact Us</Button>
+          {isMobile ? (
+            <DrawerComp/>
+          ) : (
+            <div>
+              <Button color="inherit" onClick={() => Navigate('/')}>Home</Button>
+              <Button color="inherit" onClick={() => Navigate('/gallery')}>Gallery</Button>
+              <Button color="inherit" onClick={() => Navigate('/alumni')}>Alumni</Button>
+              <Button color="inherit">About Us</Button>
+              <Button color="inherit">Testimonials</Button>
+              <Button color="inherit">Contact Us</Button>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
