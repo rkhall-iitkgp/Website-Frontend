43c856bb07b41ed3cdcfe829317c75d0f96a2585
sAksham-Ar <57193459+sAksham-Ar@users.noreply.github.com>
2023-02-01 03:20:45 +0530

    added testimonials

--- src/components/Testimonials/Data.js
+++ src/components/Testimonials/Data.js
@@ -5,13 +5,13 @@ export const Data = [
     position: "IIT Kgp '22",
   },
   {
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. (2)",
-    name: "Vinod Gupta 2",
-    position: "Founder, InfoUSA 2",
+    text:"My stay at RK were the best years of my life. The years were full of personal freedom that I enjoyed-I loved my own space, and the time with fellow RK'ites. We had DC++ which felt like we were in the future with super fast content download across learning and entertainment. RK is more like a family and built priceless memories and relationships.",
+    name: "Pawan Kumar Chandana",
+    position: "IIT Kgp '12",
   },
   {
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. (3)",
-    name: "Vinod Gupta 3",
-    position: "Founder, InfoUSA 3",
+    text: "My IIT experience would not have been complete without my RK hall stay complimenting education. As someone with a sheltered life until IIT, without trying RK Hall helped me experience true national integration with folks from all over the country (and world) with different languages and cultures cohabitating peacefully. Various extracurricular activities from self-governance, sports, music to various unique competitions helped foster teamwork that many of us could take to our careers. I made some great friends during my 5 years there (1978 - 1983) with batchmates and other contemporaries. I would not trade my 5 years in RK Hall for anything!",
+    name: "Sunil Gaitonde",
+    position: "IIT Kgp '83",
   },
 ];
