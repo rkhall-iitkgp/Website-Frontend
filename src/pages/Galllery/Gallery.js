a2e610664325dcace3f0d755a68183d44d141306
sAksham-Ar <57193459+sAksham-Ar@users.noreply.github.com>
2023-02-01 02:52:20 +0530

    fixed gallery and alumni pages

--- src/pages/Galllery/Gallery.js
+++ src/pages/Galllery/Gallery.js
@@ -2,7 +2,6 @@ import "./css/gallery.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareCaretLeft } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-import Navbar from "../../components/Navbar/Navbar";
 import Yearbox from "./yearbox";
 import Event from "./Event";
 import Eventpics from "./Eventpics";
@@ -13,104 +12,91 @@ import img3 from "./teachersday.png";
 import img4 from "./janmashtami.png";
 
 export function Gallery() {
-  window.scrollTo(0, 0);
+    const [year, setYear] = useState(2022);
+    const [yearmenu, setYearmenu] = useState(false);
+    const [event, setEvent] = useState(0);
 
-  const [year, setYear] = useState(2022);
-  const [yearmenu, setYearmenu] = useState(false);
-  const [event, setEvent] = useState(0);
+    function handleClick(year) {
+        setYear(year);
+        ((window.innerWidth < 1200 && yearmenu) || window.innerWidth >= 1200) &&
+            setEvent(0);
+        setYearmenu((prev) => !prev);
+    }
 
-  function handleClick(year) {
-    setYear(year);
-    ((window.innerWidth < 1200 && yearmenu) || window.innerWidth >= 1200) &&
-      setEvent(0);
-    window.innerWidth < 1200 && setYearmenu((prev) => !prev);
-  }
+    function handleEventClick(event) {
+        setEvent(event);
+    }
 
-  function handleEventClick(event) {
-    setEvent(event);
-  }
+    const yeargroup = [...Array(15).keys()]
+        .map((i) => 2022 - i)
+        .map((i) => (
+            <Yearbox year={i} selected={year === i} handleClick={handleClick} />
+        ));
 
-  const yeargroup = [...Array(15).keys()]
-    .map((i) => 2022 - i)
-    .map((i) => (
-      <Yearbox year={i} selected={year === i} handleClick={handleClick} />
-    ));
+    const mobileyeargroup = [...Array(15).keys()]
+        .map((i) => 2022 - i)
+        .map(
+            (i) =>
+                year !== i && (
+                    <Yearbox year={i} selected={year === i} handleClick={handleClick} />
+                )
+        );
 
-  const mobileyeargroup = [...Array(15).keys()]
-    .map((i) => 2022 - i)
-    .map(
-      (i) =>
-        year !== i && (
-          <Yearbox year={i} selected={year === i} handleClick={handleClick} />
-        )
-    );
+    return (
+        <div className="gallerypage">
+            <div className={`leftbox ${yearmenu ? "active" : ""}`}>
+                <div className="box-header">
+                    {event > 0 && <FontAwesomeIcon onClick={() => handleEventClick(0)} icon={faSquareCaretLeft} className="backevent" />}
+                    <span>Gallery</span>
+                </div>
+                <div className="years">{yeargroup}</div>
+            </div>
 
-  // const eventgroup = [
-  //   "Illumination & Rangoli",
-  //   "General Championship",
-  //   "Teachers Day",
-  //   "Intra-hall Events",
-  //   "Other Events",
-  // ].map((event) => <Event event={event} img={img1} />);
+            <div className={`years mobileyears ${yearmenu ? "active" : ""}`}>
+                {mobileyeargroup}
+            </div>
 
-  return (
-    <div className="gallerypage">
-      {/* <div className="navbar">
-        <Navbar />
-      </div> */}
+            <div className={`rightbox ${event > 0 ? "active" : ""}`}>
+                <Event
+                    id={1}
+                    event="Illumination & Rangoli"
+                    img={img1}
+                    handleEventClick={handleEventClick}
+                />
+                <Event
+                    id={2}
+                    event="General Championship"
+                    img={img2}
+                    handleEventClick={handleEventClick}
+                />
+                <Event
+                    id={3}
+                    event="Teachers Day"
+                    img={img3}
+                    handleEventClick={handleEventClick}
+                />
+                <Event
+                    id={4}
+                    event="Intra-hall Events"
+                    img={img4}
+                    handleEventClick={handleEventClick}
+                />
+                <Event
+                    id={5}
+                    event="Other Events"
+                    img={img1}
+                    handleEventClick={handleEventClick}
+                />
+            </div>
 
-      <div className={`leftbox ${yearmenu ? "active" : ""}`}>
-        <div className="box-header">
-          {event>0 && <FontAwesomeIcon onClick={() => handleEventClick(0)} icon={faSquareCaretLeft} className="backevent" />}
-          <span>Gallery</span>
+            {event === 1 && <Eventpics url="illu-rangoli" year={year} title={"Illumination & Rangoli"} />}
+            {event === 2 && <Eventpics url="gc" year={year} title={"General Championship"} />}
+            {event === 3 && <Eventpics url="teachers-day" year={year} title={"Teachers Day"} />}
+            {event === 4 && <Eventpics url="intra-hall" year={year} title={"Intra-hall Events"} />}
+            {event === 5 && <Eventpics url="others" year={year} title={"Other Events"} />}
+
         </div>
-        <div className="years">{yeargroup}</div>
-      </div>
-
-      <div className={`rightbox ${event > 0 ? "active" : ""}`}>
-        <Event
-          id={1}
-          event="Illumination & Rangoli"
-          img={img1}
-          handleEventClick={handleEventClick}
-        />
-        <Event
-          id={2}
-          event="General Championship"
-          img={img2}
-          handleEventClick={handleEventClick}
-        />
-        <Event
-          id={3}
-          event="Teachers Day"
-          img={img3}
-          handleEventClick={handleEventClick}
-        />
-        <Event
-          id={4}
-          event="Intra-hall Events"
-          img={img4}
-          handleEventClick={handleEventClick}
-        />
-        <Event
-          id={5}
-          event="Other Events"
-          img={img1}
-          handleEventClick={handleEventClick}
-        />
-      </div>
-
-      {event === 1 && <Eventpics url="illu-rangoli" year={year} title={"Illumination & Rangoli"} />}
-      {event === 2 && <Eventpics url="gc" year={year} title={"General Championship"} />}
-      {event === 3 && <Eventpics url="teachers-day" year={year} title={"Teachers Day"} />}
-      {event === 4 && <Eventpics url="intra-hall" year={year} title={"Intra-hall Events"} />}
-      {event === 5 && <Eventpics url="others" year={year} title={"Other Events"} />}
-
-      <div className={`years mobileyears ${yearmenu ? "active" : ""}`}>
-        {mobileyeargroup}
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Gallery;
