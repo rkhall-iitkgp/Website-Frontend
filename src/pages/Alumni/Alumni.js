c34295c25e8ff05530988b8327375bd3544ec3a1
Parth Nanda <parthnanda002@gmail.com>
2023-02-05 01:58:16 +0530

    alphabetical alumni

--- src/pages/Alumni/Alumni.js
+++ src/pages/Alumni/Alumni.js
@@ -6,95 +6,103 @@ import Navbar from "../../components/Navbar/Navbar";
 import Alumnus from "./Alumnus";
 import { useState } from "react";
 import axios from "axios";
-import { Audio } from 'react-loader-spinner';
+import { Audio } from "react-loader-spinner";
 
 export function Alumni() {
-    const [year, setYear] = useState(2022);
-    const [yearmenu, setYearmenu] = useState(false);
+  const [year, setYear] = useState(2022);
+  const [yearmenu, setYearmenu] = useState(false);
 
-    const [alumni, setAlumni] = useState(undefined);
+  const [alumni, setAlumni] = useState(undefined);
 
-    const url = "https://rk-backend.onrender.com/alum/data/";
+  const url = "https://rk-backend.onrender.com/alum/data/";
 
-    useEffect(() => {
-        function getAlumni() {
-            axios
-                .get(`${url}${year}`)
-                .then((response) => {
-                    const alumni = response.data;
-                    console.log(alumni);
-                    setAlumni(
-                        alumni.sort((a, b) => (
-                            a.name > b.name
-                        )).map((obj) => (
-                            <Alumnus
-                                img={'photo' in obj ? obj.photo : 'default.jpg'}
-                                name={obj.name}
-                                desc={obj.bio}
-                                linkedin={obj.link}
-                                batch={year}
-                            />
-                        ))
-                    );
-                })
-                .catch((error) => {
-                    console.log(error)
-                    setAlumni([<h1 className="loader">No records found</h1>])
-                });
-        }
-        getAlumni();
-    }, [year]);
-
-
-    function handleClick(year) {
-        setYear(year);
-        setYearmenu((prev) => !prev);
+  useEffect(() => {
+    function getAlumni() {
+      axios
+        .get(`${url}${year}`)
+        .then((response) => {
+          const alumni = response.data;
+          alumni.sort(function (a, b) {
+            if (a.name < b.name) {
+              return -1;
+            }
+            if (a.name > b.name) {
+              return 1;
+            }
+            return 0;
+          });
+          console.log(alumni);
+          setAlumni(
+            alumni.map((obj) => (
+              <Alumnus
+                img={"photo" in obj ? obj.photo : "default.jpg"}
+                name={obj.name}
+                desc={obj.bio}
+                linkedin={obj.link}
+                batch={year}
+              />
+            ))
+          );
+        })
+        .catch((error) => {
+          console.log(error);
+          setAlumni([<h1 className="loader">No records found</h1>]);
+        });
     }
+    getAlumni();
+  }, [year]);
 
-    const yeargroup = [...Array(15).keys()]
-        .map((i) => 2022 - i)
-        .map((i) => (
-            <Yearbox year={i} selected={year === i} handleClick={handleClick} />
-        ));
+  function handleClick(year) {
+    setYear(year);
+    setYearmenu((prev) => !prev);
+  }
 
-    const mobileyeargroup = [...Array(15).keys()]
-        .map((i) => 2022 - i)
-        .map(
-            (i) =>
-                year !== i && (
-                    <Yearbox year={i} selected={year === i} handleClick={handleClick} />
-                )
-        );
+  const yeargroup = [...Array(15).keys()]
+    .map((i) => 2022 - i)
+    .map((i) => (
+      <Yearbox year={i} selected={year === i} handleClick={handleClick} />
+    ));
 
-    return (
-        <>
-            <div className="navbar">
-                <Navbar />
-            </div>
-            <div className="alumpage">
-                <div className={`leftbox ${yearmenu ? "active" : ""}`}>
-                    <span className="box-header">Our Alumni</span>
-                    <div className="years">{yeargroup}</div>
-                </div>
-                <div className={`years mobileyears ${yearmenu ? "active" : ""}`}>
-                    {mobileyeargroup}
-                </div>
-                <div className="alum">
-                {
-                    alumni ? <div className="rightbox">{alumni}</div> : <Audio
-                        height="80"
-                        width="80"
-                        radius="9"
-                        color="#222436"
-                        ariaLabel="loading"
-                        wrapperStyle
-                        wrapperClass="loader"
-                    />
-                }
-                </div>
-            </div>
-        </>
+  const mobileyeargroup = [...Array(15).keys()]
+    .map((i) => 2022 - i)
+    .map(
+      (i) =>
+        year !== i && (
+          <Yearbox year={i} selected={year === i} handleClick={handleClick} />
+        )
     );
+
+  return (
+    <>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="alumpage">
+        <div className={`leftbox ${yearmenu ? "active" : ""}`}>
+          <span className="box-header">Our Alumni</span>
+          <div className="years">{yeargroup}</div>
+        </div>
+        <div className={`years mobileyears ${yearmenu ? "active" : ""}`}>
+          {mobileyeargroup}
+        </div>
+        <div className="alum">
+          {alumni ? (
+            <div className="rightbox">{alumni}</div>
+          ) : (
+            <Audio
+              height="80"
+              width="80"
+              radius="9"
+              color="#222436"
+              ariaLabel="loading"
+              wrapperStyle
+              wrapperClass="loader"
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Alumni;
