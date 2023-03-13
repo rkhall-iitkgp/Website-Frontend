3e6c4a54ceb3626c310fbcb77359a6a7379c2828
Tejas Ambhore <tejasam2020@gmail.com>
2023-03-13 08:55:38 +0530

    modulated files

--- src/components/Home_About/About.js
+++ src/components/Home_About/About.js
@@ -1,23 +1,23 @@
 import React from "react";
-import "./About.css";
+import styles from "./About.module.css";
 
 function homeAbout() {
   return (
     <div id="AboutUs" >
         <div style={{height:'10px'}}></div>
 
-    <div className="aboutUs" >
-      <div className="about_design">
-        <div className="d1"></div>
-        <div className="d2"></div>
-        <div className="d3"></div>
+    <div className={styles.aboutUs} >
+      <div className={styles.about_design}>
+        <div className={styles.d1}></div>
+        <div className={styles.d2}></div>
+        <div className={styles.d3}></div>
       </div>
 
-      <div className="aboutUs_main">
+      <div className={styles.aboutUs_main}>
         <h2>ABOUT US</h2>
 
-        <div className="aboutUs_content">
-          <div className="aboutUs_right">
+        <div className={styles.aboutUs_content}>
+          <div className={styles.aboutUs_right}>
             <h3>
             RK stands at the forefront of excellence and achievement in the campus be it Sports, SoCult, Tech or the coveted Illumination.
 
@@ -27,21 +27,21 @@ function homeAbout() {
             </p>
           </div>
 
-          <div className="aboutUs_left">
+          <div className={styles.aboutUs_left}>
             <p>
             At Radhakrishnan Hall, we take pride in our unique 'Wing Culture', where boarders of all years live together, fostering a strong sense of unity and camaraderie.
 
             </p>
-            <p className="music_room">
+            <p className={styles.music_room}>
             At RK, we believe that creativity and talent should have no boundaries. That's why we are proud to be the home of the RK Music Room, generously funded by the RK batch of 1992. Equipped with a wide range of musical instruments, this state-of-the-art infrastructure  is the perfect platform for unleashing your musical talent,which serves as a valuable resource for RKites and students of the WTMS and ETMS to hone their musical skills and prepare for events like the Inter IIT Cultural Meet.
 
             </p>
           </div>
         </div>
-        <div className="aboutUs_more">
+        <div className={styles.aboutUs_more}>
           {/* <Button name="Read More" /> */}
 
-          <div className="aboutUs_moto">
+          <div className={styles.aboutUs_moto}>
             <p>A great pLace to learn and live</p>
           </div>
           <div id="Testimonials"></div>
