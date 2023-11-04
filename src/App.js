adf6d5fe77658ce3011e60a30d66cd9245ac679b
Rohan Barsagade <rbarsagade101@gmail.com>
2023-11-04 21:51:43 +0530

    remove allumni section temporarily

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Gallery from "./pages/Galllery/Gallery";
-import Alumni from "./pages/Alumni/Alumni";
+// import Alumni from "./pages/Alumni/Alumni";
 import Home from "./pages/Home/Home";
 import Eventpics from "./pages/Galllery/Eventpics";
 
@@ -11,10 +11,10 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/gallery" element={<Gallery/>}></Route>
-        <Route path="/alumni/:year?" element={<Alumni/>}></Route>
-        <Route path="/gallery/:year/:event" element={<Eventpics/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/gallery" element={<Gallery />}></Route>
+        {/* <Route path="/alumni/:year?" element={<Alumni/>}></Route> */}
+        <Route path="/gallery/:year/:event" element={<Eventpics />}></Route>
       </Routes>
     </BrowserRouter>
   );
