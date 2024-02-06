a7f833c209f1feb0bdd837770044d1c4be8e7c3e
Anmol <anmolsoni5304@gmail.com>
2024-02-06 23:48:50 +0530

    login page  - email input

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Card from '@mui/material/Card';
-import Login_image from './Login.png'
+import Login_image from './Computer login-bro.svg'
 // import Bg_img from "bg_img.png"
 
 import TextField from '@mui/material/TextField';
@@ -9,7 +9,8 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Button from '@mui/material/Button';
 import { Typography, colors } from '@mui/material';
 import { useMediaQuery } from '@material-ui/core';
-import Divider from '@mui/material/Divider';
+import { Divider, Box } from '@mui/material';
+import { Padding } from '@mui/icons-material';
 
 
 const Login = () => {
@@ -46,26 +47,27 @@ const Login = () => {
 
                             <div style={!isMobile ? { marginLeft: '2rem', display: 'flex', flexDirection: 'column' } : { marginLeft: '1rem', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
                                 <h1 style={{ fontSize: '3rem', fontWeight: '800', fontFamily: 'sans-serif', marginTop: '2.5rem' }} className='right_heading'>Login</h1>
-                                <TextField value={email} onChange={(e) => setEmail(e.target.value)} sx={{ marginTop: '1.5rem', width: '28vmax' }} label="Email" variant="filled" />
+                                <TextField value={email} onChange={(e) => { setEmail(e.target.value); }} sx={{ marginTop: '1.5rem', width: '28vmax' }} label="Email" variant="filled" />
                                 {/* <TextField value={password} onChange={(e) => setPassword(e.target.value)} sx={{ marginTop: '1.5rem', width: '28vmax' }} label="Password" variant="filled" /> */}
                                 <Button type='submit' sx={{ marginTop: '1.5rem', background: 'black', padding: '0.8rem', width: '28vmax', '&:hover': { background: 'gray' } }} variant='contained'>Login With Password</Button>
 
-                                <Divider>
-                                    <p style={{ marginTop: '1rem' }}>or</p>
-                                </Divider>
+                                <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" style={{ marginTop: '0.8rem' }}>
+
+                                    <Divider style={{ width: '100%' }}>
+                                        <Typography variant="p">or</Typography>
+                                    </Divider>
+                                </Box>
 
                                 {/* <FormControlLabel sx={{ marginTop: '1.5rem' }} control={<Checkbox />} label="Remember Me" /> */}
 
 
 
-                                <Button type='submit' sx={{ marginTop: '1.5rem', background: 'black', width: '28vmax', '&:hover': { background: 'gray' } }} variant='contained'>Login</Button>
+                                <Button type='submit' sx={{ marginTop: '1.5rem', background: '#FBF6EA', color: 'black', width: '28vmax', '&:hover': { background: '#FFD050' } }} variant='contained'>Login With OTP</Button>
 
 
                                 <Typography sx={{ marginTop: '1.5rem' }} >Don't have an account?
-                                    <a href="/register" style={{ color: 'blue' }}>Register here</a>
+                                    <a href="/register" style={{ color: 'blue', marginLeft: '0.5rem' }}>Register here</a>
                                 </Typography>
-
-
                             </div>
                         </div>
                     </Card>
