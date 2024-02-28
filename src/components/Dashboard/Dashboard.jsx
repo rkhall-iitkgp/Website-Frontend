fecfd60e39f14acba91138747b435212d05c1335
Anmol <anmolsoni5304@gmail.com>
2024-02-29 00:03:14 +0530

    color change

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,16 +5,16 @@ import Stack from "@mui/material/Stack"
 const Dashboard = () => {
     return (
         <>
-        <Stack spacing={0} direction="row">
-            <div style={{backgroundColor: 'yellow',height: "100vh", width: "20vw"}}></div>
-            <Stack spacing={2} direction="row">
-                <div>Image</div>
-                <Stack spacing={1} direction="column">
-                    <div>Soc/Cell Name</div>
-                    <div>Role</div>
+            <Stack spacing={0} direction="row">
+                <div style={{ backgroundColor: '#FFD050', height: "100vh", width: "15vw" }}></div>
+                <Stack spacing={2} direction="row">
+                    <div>Image</div>
+                    <Stack spacing={1} direction="column">
+                        <div>Soc/Cell Name</div>
+                        <div>Role</div>
+                    </Stack>
                 </Stack>
             </Stack>
-        </Stack>
         </>
 
     );
