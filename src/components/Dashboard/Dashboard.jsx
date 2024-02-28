f56350d3002b651c10577e03c9d95ef77c8a0d8e
Ankan <93087057+ItsAnkan@users.noreply.github.com>
2024-02-28 23:57:44 +0530

    stack

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
+import Stack from "@mui/material/Stack"
 
 
 const Dashboard = () => {
     return (
-        <div></div>
+        <>
+        <Stack spacing={0} direction="row">
+            <div style={{backgroundColor: 'yellow'}}></div>
+            <Stack spacing={2} direction="row">
+                <div>Image</div>
+                <Stack spacing={1} direction="column">
+                    <div>Soc/Cell Name</div>
+                    <div>Role</div>
+                </Stack>
+            </Stack>
+        </Stack>
+        </>
 
     );
 }
